4c419df3758e5d14c01d558327d75c1a6953f198
Ilja Tkachuk <ilja@heathmont.net>
2019-02-07 14:29:21 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Protobuf.Mixfile do
   def project do
     [
       app: :exprotobuf,
-      version: "1.2.16",
+      version: "1.2.18",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       preferred_cli_env: [
