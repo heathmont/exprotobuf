6ce543f47bc6ffc23bc17331fddaf367ab0c29f7
Ilja Tkachuk <ilja@heathmont.net>
2019-02-14 14:21:33 +0200

    some validation tests for :repeated fields

--- test/protobuf/validator_test.exs
+++ test/protobuf/validator_test.exs
@@ -30,6 +30,25 @@ defmodule Protobuf.ValidatorTest do
         Msg either_msg = 19;
       }
 
+      repeated double repeated_double = 20;
+      repeated float repeated_float = 21;
+      repeated int32 repeated_int32 = 22;
+      repeated int64 repeated_int64 = 23;
+      repeated uint32 repeated_uint32 = 24;
+      repeated uint64 repeated_uint64 = 25;
+      repeated sint32 repeated_sint32 = 26;
+      repeated sint64 repeated_sint64 = 27;
+      repeated fixed32 repeated_fixed32 = 28;
+      repeated fixed64 repeated_fixed64 = 29;
+      repeated sfixed32 repeated_sfixed32 = 30;
+      repeated sfixed64 repeated_sfixed64 = 31;
+      repeated bool repeated_bool = 32;
+      repeated string repeated_string = 33;
+      repeated bytes repeated_bytes = 34;
+
+      repeated Enum repeated_enum = 35;
+      repeated Msg repeated_msg = 36;
+
       enum Enum {
         START = 0;
         STOP  = 1;
@@ -46,35 +65,65 @@ defmodule Protobuf.ValidatorTest do
   alias Schema3.Proto3
 
   setup do
+    double = -0.5
+    float = -0.5
+    int32 = IntegerTypes.max_int32()
+    int64 = IntegerTypes.max_int64()
+    uint32 = IntegerTypes.max_uint32()
+    uint64 = IntegerTypes.max_uint64()
+    sint32 = IntegerTypes.max_int32()
+    sint64 = IntegerTypes.max_int64()
+    fixed32 = IntegerTypes.max_uint32()
+    fixed64 = IntegerTypes.max_uint64()
+    sfixed32 = IntegerTypes.max_int32()
+    sfixed64 = IntegerTypes.max_int64()
+    bool = true
+    string = "hello"
+    bytes = <<255, 255, 255>>
+    enum = :START
+
+    msg = %Proto3.Msg{
+      key: "foo",
+      value: "bar"
+    }
+
     %{
       proto3: %Proto3{
-        double: -0.5,
-        float: -0.5,
-        int32: IntegerTypes.max_int32(),
-        int64: IntegerTypes.max_int64(),
-        uint32: IntegerTypes.max_uint32(),
-        uint64: IntegerTypes.max_uint64(),
-        sint32: IntegerTypes.max_int32(),
-        sint64: IntegerTypes.max_int64(),
-        fixed32: IntegerTypes.max_uint32(),
-        fixed64: IntegerTypes.max_uint64(),
-        sfixed32: IntegerTypes.max_int32(),
-        sfixed64: IntegerTypes.max_int64(),
-        bool: true,
-        string: "hello",
-        bytes: <<255, 255, 255>>,
-        enum: :START,
-        msg: %Proto3.Msg{
-          key: "foo",
-          value: "bar"
-        },
-        either: {
-          :either_msg,
-          %Proto3.Msg{
-            key: "foo",
-            value: "bar"
-          }
-        }
+        double: double,
+        float: float,
+        int32: int32,
+        int64: int64,
+        uint32: uint32,
+        uint64: uint64,
+        sint32: sint32,
+        sint64: sint64,
+        fixed32: fixed32,
+        fixed64: fixed64,
+        sfixed32: sfixed32,
+        sfixed64: sfixed64,
+        bool: bool,
+        string: string,
+        bytes: bytes,
+        enum: enum,
+        msg: msg,
+        either: {:either_msg, msg},
+        repeated_double: [double],
+        repeated_float: [float],
+        repeated_int32: [int32],
+        repeated_int64: [int64],
+        repeated_uint32: [uint32],
+        repeated_uint64: [uint64],
+        repeated_sint32: [sint32],
+        repeated_sint64: [sint64],
+        repeated_fixed32: [fixed32],
+        repeated_fixed64: [fixed64],
+        repeated_sfixed32: [sfixed32],
+        repeated_sfixed64: [sfixed64],
+        repeated_bool: [bool],
+        repeated_string: [string],
+        repeated_bytes: [bytes],
+        repeated_enum: [enum],
+        repeated_msg: [msg]
       }
     }
   end
@@ -87,30 +136,54 @@ defmodule Protobuf.ValidatorTest do
 
   test "proto3 validate! raise", %{proto3: %Proto3{} = proto3} do
     import Proto3
-    assert_raise RuntimeError, ~r/^Elixir.Protobuf.ValidatorTest.Schema3.Proto3.t has invalid value 123 of field.+$/, fn ->
-      %Proto3{proto3 | float: 123}
-      |> validate!
-    end
+
+    assert_raise RuntimeError,
+                 ~r/^Elixir.Protobuf.ValidatorTest.Schema3.Proto3.t has invalid value 123 of field.+$/,
+                 fn ->
+                   %Proto3{proto3 | float: 123}
+                   |> validate!
+                 end
   end
 
-  test "proto3 double + float", %{proto3: %Proto3{} = proto3} do
+  test "proto3 double + float", %{
+    proto3: %Proto3{repeated_double: repeated_double, repeated_float: repeated_float} = proto3
+  } do
     import Proto3
     other_erlang_type = 123
 
     [
       other_erlang_type,
-      nil
+      nil,
+      [other_erlang_type | repeated_double],
+      [nil | repeated_double],
+      [other_erlang_type | repeated_float],
+      [nil | repeated_float]
     ]
     |> Enum.each(fn val ->
       assert {:error, _} = %Proto3{proto3 | double: val} |> validate
       assert {:error, _} = %Proto3{proto3 | float: val} |> validate
+      assert {:error, _} = %Proto3{proto3 | repeated_double: val} |> validate
+      assert {:error, _} = %Proto3{proto3 | repeated_float: val} |> validate
+
+      assert {:error, _} =
+               %Proto3{proto3 | repeated_double: val} |> validate(allow_scalar_nil: true)
+
+      assert {:error, _} =
+               %Proto3{proto3 | repeated_float: val} |> validate(allow_scalar_nil: true)
     end)
 
     assert :ok = %Proto3{proto3 | double: nil} |> validate(allow_scalar_nil: true)
     assert :ok = %Proto3{proto3 | float: nil} |> validate(allow_scalar_nil: true)
   end
 
-  test "proto3 int32 + sint32 + sfixed32", %{proto3: %Proto3{} = proto3} do
+  test "proto3 int32 + sint32 + sfixed32", %{
+    proto3:
+      %Proto3{
+        repeated_int32: repeated_int32,
+        repeated_sint32: repeated_sint32,
+        repeated_sfixed32: repeated_sfixed32
+      } = proto3
+  } do
     import Proto3
     other_erlang_type = 1.23
     pos_overflow = IntegerTypes.max_int32() + 1
@@ -120,12 +193,37 @@ defmodule Protobuf.ValidatorTest do
       other_erlang_type,
       pos_overflow,
       neg_overflow,
-      nil
+      nil,
+      [other_erlang_type | repeated_int32],
+      [pos_overflow | repeated_int32],
+      [neg_overflow | repeated_int32],
+      [nil | repeated_int32],
+      [other_erlang_type | repeated_sint32],
+      [pos_overflow | repeated_sint32],
+      [neg_overflow | repeated_sint32],
+      [nil | repeated_sint32],
+      [other_erlang_type | repeated_sfixed32],
+      [pos_overflow | repeated_sfixed32],
+      [neg_overflow | repeated_sfixed32],
+      [nil | repeated_sfixed32]
     ]
     |> Enum.each(fn val ->
       assert {:error, _} = %Proto3{proto3 | int32: val} |> validate
       assert {:error, _} = %Proto3{proto3 | sint32: val} |> validate
       assert {:error, _} = %Proto3{proto3 | sfixed32: val} |> validate
+
+      assert {:error, _} = %Proto3{proto3 | repeated_int32: val} |> validate
+      assert {:error, _} = %Proto3{proto3 | repeated_sint32: val} |> validate
+      assert {:error, _} = %Proto3{proto3 | repeated_sfixed32: val} |> validate
+
+      assert {:error, _} =
+               %Proto3{proto3 | repeated_int32: val} |> validate(allow_scalar_nil: true)
+
+      assert {:error, _} =
+               %Proto3{proto3 | repeated_sint32: val} |> validate(allow_scalar_nil: true)
+
+      assert {:error, _} =
+               %Proto3{proto3 | repeated_sfixed32: val} |> validate(allow_scalar_nil: true)
     end)
 
     assert :ok = %Proto3{proto3 | int32: nil} |> validate(allow_scalar_nil: true)
@@ -133,7 +231,14 @@ defmodule Protobuf.ValidatorTest do
     assert :ok = %Proto3{proto3 | sfixed32: nil} |> validate(allow_scalar_nil: true)
   end
 
-  test "proto3 int64 + sint64 + sfixed64", %{proto3: %Proto3{} = proto3} do
+  test "proto3 int64 + sint64 + sfixed64", %{
+    proto3:
+      %Proto3{
+        repeated_int64: repeated_int64,
+        repeated_sint64: repeated_sint64,
+        repeated_sfixed64: repeated_sfixed64
+      } = proto3
+  } do
     import Proto3
     other_erlang_type = 1.23
     pos_overflow = IntegerTypes.max_int64() + 1
@@ -143,18 +248,47 @@ defmodule Protobuf.ValidatorTest do
       other_erlang_type,
       pos_overflow,
       neg_overflow,
-      nil
+      nil,
+      [other_erlang_type | repeated_int64],
+      [pos_overflow | repeated_int64],
+      [neg_overflow | repeated_int64],
+      [nil | repeated_int64],
+      [other_erlang_type | repeated_sint64],
+      [pos_overflow | repeated_sint64],
+      [neg_overflow | repeated_sint64],
+      [nil | repeated_sint64],
+      [other_erlang_type | repeated_sfixed64],
+      [pos_overflow | repeated_sfixed64],
+      [neg_overflow | repeated_sfixed64],
+      [nil | repeated_sfixed64]
     ]
     |> Enum.each(fn val ->
       assert {:error, _} = %Proto3{proto3 | int64: val} |> validate
       assert {:error, _} = %Proto3{proto3 | sint64: val} |> validate
       assert {:error, _} = %Proto3{proto3 | sfixed64: val} |> validate
+
+      assert {:error, _} = %Proto3{proto3 | repeated_int64: val} |> validate
+      assert {:error, _} = %Proto3{proto3 | repeated_sint64: val} |> validate
+      assert {:error, _} = %Proto3{proto3 | repeated_sfixed64: val} |> validate
+
+      assert {:error, _} =
+               %Proto3{proto3 | repeated_int64: val} |> validate(allow_scalar_nil: true)
+
+      assert {:error, _} =
+               %Proto3{proto3 | repeated_sint64: val} |> validate(allow_scalar_nil: true)
+
+      assert {:error, _} =
+               %Proto3{proto3 | repeated_sfixed64: val} |> validate(allow_scalar_nil: true)
     end)
 
     assert :ok = %Proto3{proto3 | int64: nil} |> validate(allow_scalar_nil: true)
     assert :ok = %Proto3{proto3 | sint64: nil} |> validate(allow_scalar_nil: true)
     assert :ok = %Proto3{proto3 | sfixed64: nil} |> validate(allow_scalar_nil: true)
   end
+
+  #
+  # TODO : all tests for :repeated fields
+  #
 
   test "proto3 uint32 + fixed32", %{proto3: %Proto3{} = proto3} do
     import Proto3
@@ -281,7 +415,10 @@ defmodule Protobuf.ValidatorTest do
     assert {:error, _} = %Proto3{proto3 | msg: %URI{}} |> validate
     assert {:error, _} = %Proto3{proto3 | msg: %Proto3.Msg{msg | key: 123}} |> validate
     assert {:error, _} = %Proto3{proto3 | msg: %Proto3.Msg{msg | key: nil}} |> validate
-    assert :ok = %Proto3{proto3 | msg: %Proto3.Msg{msg | key: nil}} |> validate(allow_scalar_nil: true)
+
+    assert :ok =
+             %Proto3{proto3 | msg: %Proto3.Msg{msg | key: nil}}
+             |> validate(allow_scalar_nil: true)
   end
 
   test "proto3 oneof", %{proto3: %Proto3{either: {:either_msg, %Proto3.Msg{} = msg}} = proto3} do
@@ -290,15 +427,19 @@ defmodule Protobuf.ValidatorTest do
     assert :ok = %Proto3{proto3 | either: {:either_string, "hello"}} |> validate
     assert {:error, _} = %Proto3{proto3 | either: {:either_string, 123}} |> validate
     assert {:error, _} = %Proto3{proto3 | either: {:either_string, nil}} |> validate
-    assert {:error, _} = %Proto3{proto3 | either: {:either_string, nil}} |> validate(allow_scalar_nil: true)
+
+    assert {:error, _} =
+             %Proto3{proto3 | either: {:either_string, nil}} |> validate(allow_scalar_nil: true)
+
     assert {:error, _} = %Proto3{proto3 | either: {:either_msg, 123}} |> validate
     assert {:error, _} = %Proto3{proto3 | either: {:either_msg, nil}} |> validate
     assert {:error, _} = %Proto3{proto3 | either: {:hello, "world"}} |> validate
-    assert {:error, _} = %Proto3{proto3 | either: {:either_msg, %Proto3.Msg{msg | key: 123}}} |> validate
+
+    assert {:error, _} =
+             %Proto3{proto3 | either: {:either_msg, %Proto3.Msg{msg | key: 123}}} |> validate
   end
 
   #
   # TODO : proto2 tests
   #
-
 end
