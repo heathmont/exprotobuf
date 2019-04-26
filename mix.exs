b9e5cd4471e6aef22b201f32b51755c12e395b19
Ilja Tkachuk <ilja@heathmont.net>
2019-04-26 13:10:32 +0300

    fix type validator

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Protobuf.Mixfile do
   def project do
     [
       app: :exprotobuf,
-      version: "1.2.25",
+      version: "1.2.26",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       preferred_cli_env: [
